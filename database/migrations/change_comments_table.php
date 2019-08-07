0f7eb48666290eb186d7e88d1aa4ee051bf508fc
veseluy-rodjer <nikolas-ja@meta.ua>
2019-08-07 11:58:30 +0300

    Update change_comments_table.php

--- database/migrations/change_comments_table.php
+++ database/migrations/change_comments_table.php
@@ -17,13 +17,12 @@ class ChangeCommentsTable extends Migration
     public function up()
     {
         Schema::table('comments', function (Blueprint $table) {	
-			$table->integer(config('comments.key_field'))->unsigned();		
-			$table->foreign(config('comments.key_field'))->references('id')->on(config('comments.key_table'));	
-			
-			if(config('comments.user')){
-				$table->integer('user_id')->unsigned()->nullable();	//разрешаем null		
-				$table->foreign('user_id')->references('id')->on('users');
-			}
+		$table->unsignedBigInteger(config('comments.key_field'));		
+		$table->foreign(config('comments.key_field'))->references('id')->on(config('comments.key_table'));	
+		if(config('comments.user')){
+			$table->unsignedBigInteger('user_id')->nullable();	//разрешаем null		
+			$table->foreign('user_id')->references('id')->on('users');
+		}
         });
     }
 
